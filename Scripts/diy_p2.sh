bfcd57b5abb5df25caadc9b488870646f352f54f
ZH <1062723226@qq.com>
2026-08-16 12:45:31 +0800

    init

--- Scripts/diy_p2.sh
+++ Scripts/diy_p2.sh
@@ -10,3 +10,4 @@ $feeds_path install -a -f -p nikki
 $feeds_path install -a -f -p gecoosac
 $feeds_path install -a -f -p ddns_go
 $feeds_path install -a -f -p socat
+$feeds_path install -a -f -p theme_argon
