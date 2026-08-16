ebaa30da94ab703266a05239091d54b32953eb5b
ZH <1062723226@qq.com>
2026-08-16 14:16:55 +0800

    init

--- Scripts/Settings.sh
+++ Scripts/Settings.sh
@@ -35,8 +35,8 @@ sed -i "s/hostname='.*'/hostname='$WRT_NAME'/g" $CFG_FILE
 echo "CONFIG_PACKAGE_luci=y" >> ./.config
 echo "CONFIG_LUCI_LANG_zh_Hans=y" >> ./.config
 echo "CONFIG_PACKAGE_luci-theme-$WRT_THEME=y" >> ./.config
-# echo "CONFIG_PACKAGE_luci-app-$WRT_THEME-config=y" >> ./.config
-# echo "CONFIG_PACKAGE_luci-i18n-$WRT_THEME-config-zh-cn=y" >> ./.config
+echo "CONFIG_PACKAGE_luci-app-$WRT_THEME-config=y" >> ./.config
+echo "CONFIG_PACKAGE_luci-i18n-$WRT_THEME-config-zh-cn=y" >> ./.config
 
 #引入私有扩展配置
 if [ -f "$GITHUB_WORKSPACE/Config/PRIVATE.txt" ]; then
